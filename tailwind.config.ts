079ce80a0886b98fca9e3edd12b9b825d35641a7
Annie Huang <annie.hn@gmail.com>
2023-09-29 18:17:46 +1000

    Fixed tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,11 @@
-import type { Config } from 'tailwindcss'
+// Have to use module.exports format, the npx generated one, will remove single quote from 'gallery' and make it not working.
+// Check git history.
+// Original syntax:
+//    const config: Config = {content: [], ...}
+//    export default config
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -13,8 +18,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      gridTemplateColumns: {
+        gallery: 'repeat(auto-fit, minmax(250px, 1fr))',
+      },
     },
   },
   plugins: [],
-}
-export default config
+};

66da0dbab39dec717186556dff9143f0a01cc8fd
Annie Huang <annie.hn@gmail.com>
2023-09-30 14:31:09 +1000

    Create /app/results/[term]/page.tsx

---
+++ src/app/results/[term]/page.tsx
@@ -0,0 +1,12 @@
+import Gallery from '@/app/components/Gallery';
+
+type Props = { params: { term: string } };
+
+export function generateMetadata({ params: { term } }: Props) {
+  return {
+    title: `Results for ${term}`,
+  };
+}
+export function SearchResults({ params: { term } }: Props) {
+  return <Gallery topic={term} />;
+}

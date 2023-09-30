3259297edfa9311598e5d9728288a9ad44ede109
Annie Huang <annie.hn@gmail.com>
2023-09-30 15:25:23 +1000

    Set the width for the image for 250px, and create another layer to wrapped the <Image> in ImgContainer.tsx

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -15,18 +15,24 @@ const ImgContainer = ({ photo }: Props) => {
   // group | group-hover:opacity-75 -> When hover on the parent, the children change opacity to 75
   // https://nextjs.org/docs/app/api-reference/components/image#placeholder. The blur image is a very nice blur image Next.js can give you while loading the image.
   // This is need to set the network to slow 3G to be able to see.
+  // <div className='h-64 bg-gray-200 rounded-xl relative overflow-hidden group'>
   return (
-    <div className='h-64 bg-gray-200 rounded-xl relative overflow-hidden group'>
-      {/* object-cover is a tailwind class */}
-      <Image
-        src={photo.src.large}
-        alt={photo.alt}
-        fill={true}
-        sizes='(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)'
-        placeholder='blur'
-        blurDataURL={photo.blurredDataUrl}
-        className='object-cover group-hover:opacity-75'
-      />
+    <div
+      className='w-[250px] justify-self-center'
+      style={{ gridRow: `span ${photoSpans}` }}
+    >
+      <div className='rounded-xl overflow-hidden group'>
+        {/* object-cover is a tailwind class */}
+        <Image
+          src={photo.src.large}
+          alt={photo.alt}
+          fill={true}
+          sizes='(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)'
+          placeholder='blur'
+          blurDataURL={photo.blurredDataUrl}
+          className='object-cover group-hover:opacity-75'
+        />
+      </div>
     </div>
   );
 };

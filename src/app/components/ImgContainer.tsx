ced018b0246056eb0c9709eaa132740bddc610fb
Annie Huang <annie.hn@gmail.com>
2023-09-30 11:21:15 +1000

    When hover on the parent, the children change opacity to 75

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -7,15 +7,16 @@ type Props = {
 
 const ImgContainer = ({ photo }: Props) => {
   // Use https://ausi.github.io/respimagelint/  to get the value for the sizes
+  // group | group-hover:opacity-75 -> When hover on the parent, the children change opacity to 75
   return (
-    <div className='h-64 bg-gray-200 rounded-xl relative overflow-hidden'>
+    <div className='h-64 bg-gray-200 rounded-xl relative overflow-hidden group'>
       {/* object-cover is a tailwind class */}
       <Image
         src={photo.src.large}
         alt={photo.alt}
         fill={true}
         sizes='(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)'
-        className='object-cover'
+        className='object-cover group-hover:opacity-75'
       />
     </div>
   );

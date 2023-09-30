bd58ae14b3f8c6e123ddd93c11f35438ae1cd83d
Annie Huang <annie.hn@gmail.com>
2023-09-30 12:51:45 +1000

    Create the addBlurredDataUrl function in getBase64.ts

--- src/lib/getBase64.ts
+++ src/lib/getBase64.ts
@@ -1,4 +1,5 @@
 import { getPlaiceholder } from 'plaiceholder';
+import { ImagesResults, Photo } from '@/models/Images';
 
 async function getBase64(imageUrl: string) {
   // https://plaiceholder.co/docs/usage#base64
@@ -17,4 +18,25 @@ async function getBase64(imageUrl: string) {
   } catch (e) {
     if (e instanceof Error) console.log(e.stack);
   }
+}
+
+export default async function addBlurredDataUrl(
+  images: ImagesResults
+): Promise<Photo[]> {
+  // https://nextjs.org/docs/app/api-reference/components/image#placeholder
+
+  // Make all requests at once instead of waiting each one - avoiding a waterfall
+  const base64Promises = images.photos.map((photo) =>
+    getBase64(photo.src.large)
+  );
+
+  // Resolve all requests in order
+  const base64Results = await Promise.all(base64Promises);
+
+  const photoWithBlur: Photo[] = images.photos.map((photo, i) => {
+    photo.blurredDataUrl = base64Results[i];
+    return photo;
+  });
+
+  return photoWithBlur;
 }

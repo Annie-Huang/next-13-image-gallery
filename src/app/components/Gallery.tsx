94f31811a779a54e7d65a3762406bbabda75cc68
Annie Huang <annie.hn@gmail.com>
2023-09-30 12:57:48 +1000

    Call addBlurredDataUrl inside the <Gallery> component

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,6 +1,7 @@
 import type { ImagesResults } from '@/models/Images';
 import fetchImages from '@/lib/fetchImages';
 import ImgContainer from './ImgContainer';
+import addBlurredDataUrl from '@/lib/getBase64';
 
 export default async function Gallery() {
   // https://www.pexels.com/api/documentation/#photos-curated
@@ -11,10 +12,13 @@ export default async function Gallery() {
   if (!images)
     return <h2 className='m-4 text-2xl font-bold'>No Images Found</h2>;
 
+  const photosWithBlur = await addBlurredDataUrl(images);
+
   // grid-cols-gallery is defined in C:\react\next-13-image-gallery\tailwind.config.ts
   return (
     <section className='px-2 my-3 grid gap-2 grid-cols-gallery'>
-      {images.photos.map((photo) => (
+      {/*{images.photos.map((photo) => (*/}
+      {photosWithBlur.map((photo) => (
         <ImgContainer photo={photo} key={photo.id} />
       ))}
     </section>

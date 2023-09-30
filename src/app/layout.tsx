d1473973e89d9bb7093d41ab26f16afafa5ff1fe
Annie Huang <annie.hn@gmail.com>
2023-09-30 14:51:20 +1000

    Revalidate the page every 1 hr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import './globals.css';
 import type { Metadata } from 'next';
 import Navbar from './components/Navbar';
 
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate
+export const revalidate = 3600; // 1 hr
+
 export const metadata: Metadata = {
   title: 'Next.js Image Gallery',
   description: 'Net Ninja tutorial series by Dave Gray',

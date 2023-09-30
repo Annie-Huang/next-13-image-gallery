4c13137d1dc7f99b81d69e7cdd0103c42c01e9db
Annie Huang <annie.hn@gmail.com>
2023-09-30 17:58:42 +1000

    Create <Footer>. Create logic to check whether we display prev|next Page area

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+type Props = {
+  topic: string;
+  page?: string;
+  prevPage: string | null;
+  nextPage: string | null;
+};
+
+export default function Footer({ topic, page, nextPage, prevPage }: Props) {
+  if (!prevPage && !nextPage) return;
+
+  const pageNums: number[] = [];
+  if (prevPage && nextPage) {
+    for (let i = parseInt(prevPage) + 1; i < parseInt(nextPage); i++) {
+      pageNums.push(i);
+    }
+  }
+
+  const nextPageArea = nextPage ? (
+    <Link
+      href={`/results/${topic}/${nextPage}`}
+      className={!prevPage ? 'mx-auto' : ''}
+    >
+      {!prevPage ? 'more' : ''} &gt;&gt;&gt;
+    </Link>
+  ) : null;
+
+  const prevPageArea = prevPage ? (
+    <Link
+      href={`/results/${topic}/${nextPage}`}
+      className={!nextPage ? 'mx-auto' : ''}
+    >
+      &lt;&lt;&lt; {!nextPage ? 'back' : null}
+    </Link>
+  ) : null;
+
+  return <div></div>;
+}

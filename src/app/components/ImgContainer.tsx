362f19f8ee54442453e73265a08a2b8ac737a3f7
Annie Huang <annie.hn@gmail.com>
2023-09-30 15:35:22 +1000

    Create the <Link> to wrap the <Image> so it can direct user to go to the pexel site the full image. Delete fill and object-cover, and set the size to be 250px for the <Image>

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -1,5 +1,6 @@
 import type { Photo } from '@/models/Images';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {
   photo: Photo;
@@ -21,18 +22,30 @@ const ImgContainer = ({ photo }: Props) => {
       className='w-[250px] justify-self-center'
       style={{ gridRow: `span ${photoSpans}` }}
     >
-      <div className='rounded-xl overflow-hidden group'>
-        {/* object-cover is a tailwind class */}
-        <Image
-          src={photo.src.large}
-          alt={photo.alt}
-          fill={true}
-          sizes='(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)'
-          placeholder='blur'
-          blurDataURL={photo.blurredDataUrl}
-          className='object-cover group-hover:opacity-75'
-        />
-      </div>
+      <Link
+        href={photo.url}
+        target='_blank'
+        className='grid place-content-center'
+      >
+        <div className='rounded-xl overflow-hidden group'>
+          {/* object-cover is a tailwind class
+              Next.js can use the full width and height to calculate the aspect-ratio.
+          */}
+          <Image
+            src={photo.src.large}
+            alt={photo.alt}
+            // fill={true}
+            width={photo.width}
+            height={photo.height}
+            // sizes='(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)'
+            sizes='250px'
+            placeholder='blur'
+            blurDataURL={photo.blurredDataUrl}
+            // className='object-cover group-hover:opacity-75'
+            className='group-hover:opacity-75'
+          />
+        </div>
+      </Link>
     </div>
   );
 };

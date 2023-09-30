d0544ab41434259b76da96be0b26393df4b309e8
Annie Huang <annie.hn@gmail.com>
2023-09-30 17:12:05 +1000

    Change logic of showing not image found message. Create place holder for pagination logic in <Gallery>

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -27,20 +27,25 @@ export default async function Gallery({ topic = 'curated', page }: Props) {
 
   const images: ImagesResults | undefined = await fetchImages(url);
 
-  if (!images)
+  if (!images || images.per_page === 0)
     return <h2 className='m-4 text-2xl font-bold'>No Images Found</h2>;
 
   const photosWithBlur = await addBlurredDataUrl(images);
+
+  // calculate pagination
 
   // grid-cols-gallery is defined in C:\react\next-13-image-gallery\tailwind.config.ts
   // auto-rows-[10px] means grid row by default is 10px height
   return (
     // <section className='px-2 my-3 grid gap-2 grid-cols-gallery auto-rows-[10px]'>
-    <section className='px-1 my-3 grid grid-cols-gallery auto-rows-[10px]'>
-      {/*{images.photos.map((photo) => (*/}
-      {photosWithBlur.map((photo) => (
-        <ImgContainer photo={photo} key={photo.id} />
-      ))}
-    </section>
+    <>
+      <section className='px-1 my-3 grid grid-cols-gallery auto-rows-[10px]'>
+        {/*{images.photos.map((photo) => (*/}
+        {photosWithBlur.map((photo) => (
+          <ImgContainer photo={photo} key={photo.id} />
+        ))}
+      </section>
+      {/* Add footer */}
+    </>
   );
 }

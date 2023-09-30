33bb89fdc15e25df215c7c74c7493e454d80bd17
Annie Huang <annie.hn@gmail.com>
2023-09-30 11:15:03 +1000

    Use https://ausi.github.io/respimagelint/  to get the value for the sizes

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -6,6 +6,7 @@ type Props = {
 };
 
 const ImgContainer = ({ photo }: Props) => {
+  // Use https://ausi.github.io/respimagelint/  to get the value for the sizes
   return (
     <div className='h-64 bg-gray-200 rounded-xl relative overflow-hidden'>
       {/* object-cover is a tailwind class */}
@@ -13,6 +14,7 @@ const ImgContainer = ({ photo }: Props) => {
         src={photo.src.large}
         alt={photo.alt}
         fill={true}
+        sizes='(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)'
         className='object-cover'
       />
     </div>
